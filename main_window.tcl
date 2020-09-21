e2b1f84acb52670b330ddaa11095d3c56e811c55
mark-summerfield <mark@qtrac.eu>
2020-09-21 13:40:20 +0100

    more

--- main_window.tcl
+++ main_window.tcl
@@ -11,7 +11,6 @@ namespace eval main_window {
     variable high_score 0
 
     proc make_window {} {
-        ttk::frame .main
         make_widgets
         make_layout
         make_bindings
@@ -20,27 +19,38 @@ namespace eval main_window {
 
 
     proc make_widgets {} {
+        ttk::frame .main
         ttk::frame .main.toolbar
-        tk::canvas .main.canvas
+        ttk::button .main.toolbar.new -text New -style Toolbutton \
+            -command main_window::on_new
+        tk::canvas .main.canvas -background "#FFFEE0"
         ttk::label .main.status_label
-        # TODO
-        puts make_widgets
+        # TODO configure
     }
 
 
     proc make_layout {} {
+        grid .main -sticky nsew
         grid .main.toolbar -sticky ew
-        grid .main.canvas -sticky nsew
+        grid .main.toolbar.new -sticky w
+        grid .main.canvas -sticky nsew -pady $const::PAD
         grid .main.status_label -sticky ew
+        grid columnconfigure .main 0 -weight 1
+        grid rowconfigure .main 1 -weight 1
         grid columnconfigure . 0 -weight 1
-        grid rowconfigure . 1 -weight 1
-        puts make_layout ;# TODO
+        grid rowconfigure . 0 -weight 1
     }
 
 
     proc make_bindings {} {
         bind . <Control-q> { main_window::on_quit }
         bind . <Escape> { main_window::on_quit }
+        # TODO complete
+    }
+
+
+    proc on_new {} {
+        puts "on_new" ;# TODO
     }
 
 
